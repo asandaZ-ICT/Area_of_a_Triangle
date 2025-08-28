bcb6d49173e659a0c6c1ac3249ca01b312d3a0aa
Asanda Zondo <asandazondo884@gmail.com>
2025-08-28 04:36:23 +0200

    Add files via upload

---
+++ area_of_a_Triangle.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Write the height : ");
+double height =double.Parse(Console.ReadLine());
+
+Console.Write("Write the base : ");
+double baseLength =double.Parse(Console.ReadLine());
+
+double answer = (0.5 * baseLength * height );
+Console.WriteLine($"the area of a triangle is {answer}");
